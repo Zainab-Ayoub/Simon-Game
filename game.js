36797350010b306aa4b1354627d4218bafc7edcb
Zainab-Ayoub <151748568+Zainab-Ayoub@users.noreply.github.com>
2024-08-31 22:32:27 +0500

    Add files via upload

---
+++ game.js
@@ -0,0 +1,19 @@
+
+var buttonColour= ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+
+function nextSequence(){
+
+    var randomNumber = Math.floor(Math.random()*4);
+    var randomChosenColor = buttonColour[randomNumber];
+    gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+    audio.play();
+
+}
+   
+nextSequence();
